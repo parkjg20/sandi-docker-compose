fa5285771c93dae7ae2061651a92f69595cc77bd
parkjg20 <parkgj20@naver.com>
2024-01-25 11:17:01 +0900

    샌디 api 공유용 docker compose 내용:

---
+++ docker-export.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/Desktop/sdc-api
+docker save -o ~/Desktop/sdc-api/api.tar api
+docker save -o ~/Desktop/sdc-api/pts.tar pts
+docker save -o ~/Desktop/sdc-api/bds.tar bds
+docker save -o ~/Desktop/sdc-api/pnc.tar pnc
+docker save -o ~/Desktop/sdc-api/mbs.tar mbs
+docker save -o ~/Desktop/sdc-api/pus.tar pus
+docker save -o ~/Desktop/sdc-api/fms.tar fms
+docker save -o ~/Desktop/sdc-api/cos.tar cos
+docker save -o ~/Desktop/sdc-api/iot.tar iot
---
+++ docker-import.sh
@@ -0,0 +1,10 @@
+
+docker load -i ~/Desktop/sdc-api/api.tar
+docker load -i ~/Desktop/sdc-api/pts.tar
+docker load -i ~/Desktop/sdc-api/bds.tar
+docker load -i ~/Desktop/sdc-api/pnc.tar
+docker load -i ~/Desktop/sdc-api/mbs.tar
+docker load -i ~/Desktop/sdc-api/pus.tar
+docker load -i ~/Desktop/sdc-api/fms.tar
+docker load -i ~/Desktop/sdc-api/cos.tar
+docker load -i ~/Desktop/sdc-api/iot.tar
