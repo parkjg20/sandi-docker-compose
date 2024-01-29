2d955f6fcff00a6302fd082efdc8dd30b75bb7cb
parkjg20 <parkjg20@tosky.co.kr>
2024-01-29 15:18:11 +0900

    sandi docker -> bin으로 통합 관리할 예정

---
+++ sandi/sandi-gradle-build.sh
@@ -0,0 +1,31 @@
+echo "SANDI Gradle 빌드를 시작합니다.";
+
+echo "- snicorp-sdc-api 빌드 시작 -";
+cd ~/Desktop/workspace/sdc/snicorp-sdc-api && ./gradlew clean build;
+
+echo "- snicorp-sdc-cos 빌드 시작 -";
+cd ~/Desktop/workspace/sdc/snicorp-sdc-cos && ./gradlew clean build;
+
+echo "- snicorp-sdc-bds 빌드 시작 -";
+cd ~/Desktop/workspace/sdc/snicorp-sdc-bds && ./gradlew clean build;
+
+echo "- snicorp-sdc-fms 빌드 시작 -";
+cd ~/Desktop/workspace/sdc/snicorp-sdc-fms && ./gradlew clean build;
+
+echo "- snicorp-sdc-iot 빌드 시작 -";
+cd ~/Desktop/workspace/sdc/snicorp-sdc-iot && ./gradlew clean build;
+
+echo "- snicorp-sdc-mbs 빌드 시작 -";
+cd ~/Desktop/workspace/sdc/snicorp-sdc-mbs && ./gradlew clean build;
+
+echo "- snicorp-sdc-pnc 빌드 시작 -";
+cd ~/Desktop/workspace/sdc/snicorp-sdc-pnc && ./gradlew clean build;
+
+echo "- snicorp-sdc-pts 빌드 시작 -";
+cd ~/Desktop/workspace/sdc/snicorp-sdc-pts && ./gradlew clean build;
+
+echo "- snicorp-sdc-pus 빌드 시작 -";
+cd ~/Desktop/workspace/sdc/snicorp-sdc-pus && ./gradlew clean build;
+
+
+echo "SANDI Gradle 빌드를 완료했습니다.";
