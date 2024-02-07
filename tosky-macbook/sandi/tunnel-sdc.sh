2780a31fd2c633d240c33f561638284e5f186cfe
parkjg20 <parkjg20@naver.com>
2024-02-07 09:29:11 +0900

    EIP 적용으로 인해 아이피 수정

--- tosky-macbook/sandi/tunnel-sdc.sh
+++ tosky-macbook/sandi/tunnel-sdc.sh
@@ -1,3 +1,3 @@
-ssh -i ~/.ssh/hpa/keypair-dev-sdc-bastionhost-an2.cer -L 25432:rds-dev-sdc-bds-an2.ciizug3wkvtz.ap-northeast-2.rds.amazonaws.com:5432 -L 6379:redis-dev-sdc-cos-an2.yqnskf.clustercfg.apn2.cache.amazonaws.com:6379 -N -f ec2-user@3.38.178.188
+ssh -i ~/.ssh/hpa/keypair-dev-sdc-bastionhost-an2.cer -L 25432:rds-dev-sdc-bds-an2.ciizug3wkvtz.ap-northeast-2.rds.amazonaws.com:5432 -L 6379:redis-dev-sdc-cos-an2.yqnskf.clustercfg.apn2.cache.amazonaws.com:6379 -N -f ec2-user@52.79.238.97
 
-ssh -i ~/.ssh/hpa/keypair-prd-sdc-bastionhost-an2.cer -L 15432:rds-prd-sdc-bds-an2.ciizug3wkvtz.ap-northeast-2.rds.amazonaws.com:5432 -L 16379:redis-prd-sdc-cos-an2.yqnskf.clustercfg.apn2.cache.amazonaws.com:6379  -N -f ec2-user@3.38.97.250
+ssh -i ~/.ssh/hpa/keypair-prd-sdc-bastionhost-an2.cer -L 15432:rds-prd-sdc-bds-an2.ciizug3wkvtz.ap-northeast-2.rds.amazonaws.com:5432 -L 16379:redis-prd-sdc-cos-an2.yqnskf.clustercfg.apn2.cache.amazonaws.com:6379  -N -f ec2-user@15.164.43.128
