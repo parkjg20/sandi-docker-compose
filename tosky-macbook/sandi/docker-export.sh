05ea19d2359410a3bcfc3cf5d9c39d293f64d018
parkjg20 <parkjg20@naver.com>
2024-02-13 13:44:32 +0900

    docker-export 후 압축 프로세스까지 적용

--- tosky-macbook/sandi/docker-export.sh
+++ tosky-macbook/sandi/docker-export.sh
@@ -1,4 +1,5 @@
-mkdir -p ~/Desktop/sdc-api
+mkdir -p ~/Desktop/sdc-api;
+
 docker save -o ~/Desktop/sdc-api/api.tar api
 docker save -o ~/Desktop/sdc-api/pts.tar pts
 docker save -o ~/Desktop/sdc-api/bds.tar bds
@@ -8,3 +9,9 @@ docker save -o ~/Desktop/sdc-api/pus.tar pus
 docker save -o ~/Desktop/sdc-api/fms.tar fms
 docker save -o ~/Desktop/sdc-api/cos.tar cos
 docker save -o ~/Desktop/sdc-api/iot.tar iot
+
+nanos=$(date +%s%3);
+fileName="sdc-api-${nanos}.zip"
+zip -r ~/Desktop/$fileName ~/Desktop/sdc-api;
+
+echo "Export and zipped $fileName";
