17ce8b956fd79011ceabc6f73b76a24e7dc92e36
parkjg20 <parkjg20@naver.com>
2024-02-13 13:20:28 +0900

    실행권한 수정

--- tosky-macbook/sandi/sandi-checkout.sh
+++ tosky-macbook/sandi/sandi-checkout.sh
@@ -20,12 +20,17 @@ for dir in "${directories[@]}"; do
     cd $dir
 
     # 브랜치가 존재하는지 확인
-    if git show-ref --verify --quiet refs/heads/$branch; then
+    if git rev-parse --verify "$branch" > /dev/null 2>&1; then
         # 브랜치가 존재하면 체크아웃
-        git checkout $branch
-        echo "Checked out to branch $branch in $dir"
+        echo "Switching to existing branch '$branch'"
+        git checkout "$branch"
+    elif git ls-remote --heads origin "$branch" | grep "$branch" > /dev/null; then
+        # local에는 없으나 remote에 있으면 checkout
+        echo "Branch '$branch' found in remote. Checking out and tracking..."
+        git checkout --track origin/"$branch"
     else
-        # 브랜치가 존재하지 않으면 경고 메시지 출력
-        echo "Branch '$branch' does not exist in $dir."
+        # 브랜치가 존재하지 않으면 현재 브랜치 기준으로 경고 메시지 출력
+        echo "Branch '$branch' not found. Creating and switching to new branch..."
+        git checkout -b "$branch"
     fi
 done
